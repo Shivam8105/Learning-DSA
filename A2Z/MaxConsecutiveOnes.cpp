280c77ef7d4cc6dcd1146cdd22368e1290ecff6f
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2025-07-24 23:18:32 +0530

    updated

---
+++ A2Z/MaxConsecutiveOnes.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int findMaxConsecutiveOnes(vector<int>&nums){
+    int count = 0;
+    int maxCount = 0;
+    for(int i = 0; i < nums.size(); i++){
+        if(nums[i] == 1){
+            count++;
+            if(count >= maxCount){
+                maxCount = count;
+            }
+        }else{
+            count = 0;
+        }
+    }
+    return maxCount;
+}
+
+int main(){
+    vector<int> v = {1,0,1,1,1,0};
+    int x = findMaxConsecutiveOnes(v);
+    cout << x << endl;
+}
