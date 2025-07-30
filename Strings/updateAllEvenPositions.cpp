09776e1af71121384639a24d20f7df5b39d3403b
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2025-07-30 20:01:24 +0530

    updated

---
+++ Strings/updateAllEvenPositions.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string str;
+    getline(cin,str);
+    cout << str.length() << endl;
+    for(int i = 0; i < str.length(); i++){
+        if( i % 2 == 0){
+            str[i] = 'a';
+        }
+    }
+    cout << str << endl;
+}
