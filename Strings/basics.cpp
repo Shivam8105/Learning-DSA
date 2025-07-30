4629a1ca7fba33aa84f9beffcbce0ed63fc80f74
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2025-07-30 19:45:27 +0530

    updated

---
+++ Strings/basics.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    // char str[] = "Hello";
+    // cout << str;
+    // char str1[] = " World";
+    // cout << strcat(str,str1) << endl;
+    // cout << strlen(str1) << endl;
+    // string str;
+    // getline(cin,str);
+    // cout << str;
+
+    string name;
+    getline(cin, name);  // Skips input!
+    cout << name;
+}
