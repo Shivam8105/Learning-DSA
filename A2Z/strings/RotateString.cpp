fd5ad3a6ba301173b59239645df423afd4de1e2a
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2025-08-05 17:11:32 +0530

    updated

--- A2Z/strings/RotateString.cpp
+++ A2Z/strings/RotateString.cpp
@@ -1,10 +1,33 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+// Your function definition
 bool rotateString(string s, string goal) {
-        if (s.length() != goal.length())
-            return false;
-        for (int i = 0; i < s.length(); i++) {
-            rotate(s.begin(), s.begin() + 1, s.end());
-            if (s == goal)
-                return true;
-        }
+    if (s.length() != goal.length())
         return false;
+    for (int i = 0; i < s.length(); i++) {
+        rotate(s.begin(), s.begin() + 1, s.end());
+        if (s == goal)
+            return true;
     }
+    return false;
+}
+
+int main() {
+    string s, goal;
+    cout << "Enter the first string: ";
+    cin >> s;
+    cout << "Enter the goal string: ";
+    cin >> goal;
+
+    if (rotateString(s, goal)) {
+        cout << "True: The goal is a rotation of s." << endl;
+    } else {
+        cout << "False: The goal is NOT a rotation of s." << endl;
+    }
+
+    return 0;
+}
