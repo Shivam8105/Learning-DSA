08028c49ca5fafdd882b3912d4ac9761913b75d2
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2025-08-13 18:30:31 +0530

    updated

--- recursion/printSum.cpp
+++ recursion/printSum.cpp
@@ -9,6 +9,15 @@ void add(int sum, int k){
     add(sum + k, k - 1); 
 }
 
+int sum(int n){
+    if(n == 0){
+        return 0;
+    }
+    return n + sum(n - 1);
+}
+
 int main(){
     add(0,10);
+    cout << endl;
+    cout << sum(10) << endl;
 }
