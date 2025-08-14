efa990a0d8216e867de6a41dadc160f95f333be4
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2025-08-14 15:51:26 +0530

    updated

--- recursion/uniquePaths.cpp
+++ recursion/uniquePaths.cpp
@@ -2,16 +2,29 @@
 
 using namespace std;
 
-int uniquePaths(int m, int n)
-{
-    if (m == 1 || n == 1)
-        return 1;
-    return uniquePaths(m, n - 1) + uniquePaths(m - 1, n);
+// int uniquePaths(int m, int n)
+// {
+//     if (m == 1 || n == 1)
+//         return 1;
+//     return uniquePaths(m, n - 1) + uniquePaths(m - 1, n);
+// }
+
+void uniquePaths(int m, int n, string s){
+   if(m == 1 && n == 1){
+    cout << s << endl;
+    return;
+   }
+   if(n > 1){
+    uniquePaths(m, n - 1, s + "R");
+   }
+   if(m > 1){
+    uniquePaths(m - 1,n,s + "D");
+   }
 }
 
 int main()
 {
     int m, n;
     cin >> m >> n;
-    cout << uniquePaths(m,n) << endl;
+    uniquePaths(m,n,"");
 }
