aa84118792c57ac86b8d04518ccff0841e36df82
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2025-08-15 19:56:21 +0530

    updated

--- recursion/stairPath.cpp
+++ recursion/stairPath.cpp
@@ -7,8 +7,16 @@ int stair(int n){
     return stair(n - 1) + stair(n - 2);
 }
 
+int stair3(int n){
+    if(n == 1) return 1;
+    if(n == 2) return 2;
+    if(n == 3) return 4;
+    return stair3(n - 3) + stair3(n - 2) + stair3(n - 1); 
+}
+
 int main(){
     int n;
     cin >> n;
     cout << stair(n) << endl;
+    cout << stair3(n) << endl;
 }
