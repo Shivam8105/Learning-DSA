dd40a769c33c3609db477213e877e6f5dd3eda2f
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2025-09-30 14:18:31 +0530

    updated

--- BinarySearch/minimumTimeToCompleteTrips.cpp
+++ BinarySearch/minimumTimeToCompleteTrips.cpp
@@ -3,18 +3,50 @@
 
 using namespace std;
 
-long long minimumTime(vector<int>& time, int totalTrips) {
-    int n = time.size();
-    long long trip_time = 1;
-    while (true) {
-        long long trips = 0;
-        for (int i = 0; i < n; i++) {
-            trips += trip_time / time[i];
-        }
-        if (trips >= totalTrips) return trip_time;
-        trip_time++;
+// long long minimumTime(vector<int>& time, int totalTrips) {
+//     int n = time.size();
+//     long long trip_time = 1;
+//     while (true) {
+//         long long trips = 0;
+//         for (int i = 0; i < n; i++) {
+//             trips += trip_time / time[i];
+//         }
+//         if (trips >= totalTrips) return trip_time;
+//         trip_time++;
+//     }
+//     return -1;
+// }
+
+bool check(long long mid, vector<int>& time, int totalTrips){
+    long long trips = 0;
+    for(int i = 0; i < time.size(); i++){
+        trips += mid / time[i];
+        if(trips >= totalTrips) return true;
     }
-    return -1;
+    return false;
+}
+
+long long minimumTime(vector<int>& time, int totalTrips){
+    int mx = -1;
+    for(int i = 0; i < time.size(); i++){
+        if(time[i] > mx){
+            mx = time[i];
+        }
+    }
+    long long low = 1;
+    long long high = (long long) mx * totalTrips;
+    long long ans = -1;
+
+    while(low <= high){
+        int mid = low + (high - low) / 2;
+        if(check(mid,time,totalTrips)){
+            ans = mid;
+            high = mid - 1;
+        }else{
+            low = mid + 1;
+        }
+    }
+    return ans;
 }
 
 
