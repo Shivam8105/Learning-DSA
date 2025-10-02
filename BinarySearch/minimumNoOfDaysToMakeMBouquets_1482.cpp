bbd871cc73ab4566262bab3156f115980d0aaa31
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2025-10-03 00:46:26 +0530

    updated

--- BinarySearch/minimumNoOfDaysToMakeMBouquets_1482.cpp
+++ BinarySearch/minimumNoOfDaysToMakeMBouquets_1482.cpp
@@ -1,45 +1,58 @@
 #include <iostream>
 using namespace std;
 
-    bool check(vector<int>& bloomDay, int m, int k, int day) {
-        int flower = 0;
-        int bouquet = 0;
+bool check(vector<int> &bloomDay, int m, int k, int day)
+{
+    int flower = 0;
+    int bouquet = 0;
 
-        for(int i = 0; i < bloomDay.size(); i++){
-            if(bloomDay[i] <= day){
-                flower++;
-            }else{
-                bouquet += flower / k;
-                flower = 0;
-            }
+    for (int i = 0; i < bloomDay.size(); i++)
+    {
+        if (bloomDay[i] <= day)
+        {
+            flower++;
         }
-        bouquet += flower / k;
-        return bouquet >= m;
-    }
-
-    int minDays(vector<int>& bloomDay, int m, int k) {
-         if ((long long)m * k > bloomDay.size()) return -1;
-        int low = 1LL * (*min_element(bloomDay.begin(), bloomDay.end()));
-        int high = 1LL * (*max_element(bloomDay.begin(), bloomDay.end()));
-        int ans = -1;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (check(bloomDay, m, k, mid)) {
-                ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
+        else
+        {
+            bouquet += flower / k;
+            flower = 0;
         }
-        return ans;
     }
+    bouquet += flower / k;
+    return bouquet >= m;
+}
 
-int main(){
+int minDays(vector<int> &bloomDay, int m, int k)
+{
+    if ((long long)m * k > bloomDay.size())
+        return -1;
+    int low = 1LL * (*min_element(bloomDay.begin(), bloomDay.end()));
+    int high = 1LL * (*max_element(bloomDay.begin(), bloomDay.end()));
+    int ans = -1;
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (check(bloomDay, m, k, mid))
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
     int n;
     cin >> n;
     vector<int> bloomDay;
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++)
+    {
         int x;
         cin >> x;
         bloomDay.push_back(x);
@@ -49,6 +62,6 @@ int main(){
     int k;
     cin >> k;
 
-    int minimumDays = minDays(bloomDay,m,k);
+    int minimumDays = minDays(bloomDay, m, k);
     cout << minimumDays << endl;
 }
