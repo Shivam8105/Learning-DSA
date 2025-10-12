9ceb5f39ab21894e4123002e571ddc72f65a3dd4
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2025-10-13 01:19:15 +0530

    updated

--- BitManipulation/checkTheIthBitIsSetOrNot.cpp
+++ BitManipulation/checkTheIthBitIsSetOrNot.cpp
@@ -1,15 +1,36 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int n;
-    cin >> n;
-    int i;
-    cin >> i;
+//! bit mask method
 
-    if((n & (1 << i)) != 0){
-        cout << "True" << endl;
-    }else{
-        cout << "False" << endl;
-    }
-}
+// int main(){
+//     int n;
+//     cin >> n;
+//     int i;
+//     cin >> i;
+
+//     if((n & (1 << i)) != 0){
+//         cout << "True" << endl;
+//     }else{
+//         cout << "False" << endl;
+//     }
+// }
+
+//! division method
+
+// int main(){
+//     int n;
+//     cin >> n;
+//     int i;
+//     cin >> i;
+
+//     if((n >> i) % 2 != 0){
+//         cout << "True" << endl;
+//     }else{
+//         cout << "False" << endl;
+//     }
+// }
+
+
+//! naive approach - convert into binary number and then start from back to the ith position & if it's 1 then the bit is set.
+
