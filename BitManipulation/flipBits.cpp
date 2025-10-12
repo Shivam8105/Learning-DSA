7885d34da6b0a665f2a2fb4350bdcd3d455c308a
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2025-10-12 07:07:26 +0530

    updated

---
+++ BitManipulation/flipBits.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int leftShift = (n << 1) - 1;
+    int ans = n ^ leftShift;
+    cout << ans << endl;
+    return 0;
+}
