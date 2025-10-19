55f71a5db03c9d27379f21527cb511d59a6088c8
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2025-10-19 13:55:16 +0530

    updated

--- BitManipulation/find2UniqueNumbers.cpp
+++ BitManipulation/find2UniqueNumbers.cpp
@@ -12,27 +12,19 @@ int main() {
         cin >> x;
         nums.push_back(x);
     }
-
-    // Step 1: XOR all elements. The result will be a ^ b, where a and b are the unique numbers.
     int xor_all = 0;
     for(int i = 0; i < n; i++) {
         xor_all ^= nums[i];
     }
-
-    // Step 2: Find the rightmost set bit in xor_all (a ^ b)
     int rightmost_set_bit = xor_all & -xor_all;
-
-    // Step 3: Divide numbers into two groups based on the rightmost set bit
     int num1 = 0, num2 = 0;
     for(int i = 0; i < n; i++) {
         if(nums[i] & rightmost_set_bit) {
-            num1 ^= nums[i];  // Group 1: bit is set
+            num1 ^= nums[i];  
         } else {
-            num2 ^= nums[i];  // Group 2: bit is not set
+            num2 ^= nums[i];  
         }
     }
-
-    // Output the two unique numbers
     cout << num1 << " " << num2 << endl;
     return 0;
 }
