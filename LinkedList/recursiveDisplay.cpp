324ef859c031af2e191198e5e5a4989dbd03d5b4
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2025-10-25 00:05:38 +0530

    updated

---
+++ LinkedList/recursiveDisplay.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void displayRecursive(Node* head) {
+    if (head == NULL)
+        return;
+    cout << head->data << " ";
+    displayRecursive(head->next);
+}
+
+int main() {
+    Node* a = new Node(10);
+    Node* b = new Node(20);
+    Node* c = new Node(30);
+    Node* d = new Node(40);
+    a->next = b;
+    b->next = c;
+    c->next = d;
+
+    displayRecursive(a);
+    cout << endl;
+
+    return 0;
+}
