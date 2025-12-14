515479f7249af262d224e008b859661b41cf7dab
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2025-12-14 20:02:30 +0530

    updated

---
+++ Dynamic Programming/removingDigits.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+vector<int> get_digits(int n){
+    vector<int> result;
+    while(n > 0){
+        if(n % 10 != 0){
+            result.push_back(n % 10);
+        }
+        n /= 10;
+    }
+    return result;
+}
+vector<int> dp;
+int solve(int n){
+    if(n == 0) return 0;
+    if(n <= 9) return 1;
+    if(dp[n] != -1) return dp[n];
+    vector<int> d = get_digits(n);
+
+    int result = INT_MAX;
+
+    for(int i = 0; i < d.size(); i++){
+        result = min(result, solve(n - d[i]));
+    }
+
+    return dp[n] = result + 1;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    dp.resize(1000005,-1);
+    cout << solve(n) << endl;
+}
--- Dynamic Programming/weirdAlgorithm.cpp
+++ Dynamic Programming/weirdAlgorithm.cpp
@@ -16,10 +16,22 @@ int solve(int n){
     return dp[n] = 1 + min({solve(n - 1),(n % 2 == 0) ? solve(n / 2) : inf,(n % 3 == 0) ? solve(n / 3) : inf});
 }
 
+
+
+int sol(int n,vector<int>& dp){
+    dp[1] = 0;
+    dp[2] = 1;
+    dp[3] = 1;
+    for(int i = 4; i <= n; i++){
+        dp[i] = 1 + min({dp[i - 1],(i % 2 == 0) ? dp[i / 2] : inf,(i % 3 == 0) ? dp[i / 3] : inf});
+    }
+    return dp[n];
+}
+
 int main(){
     int n;
     cin >> n;
     dp.resize(n + 1,-1);
-
+    cout << sol(n,dp) << endl;
     cout << solve(n) << endl;
 }
