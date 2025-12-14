96a378aa7e03aebf55a92f74b30ee264a66cbc4f
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2025-12-14 18:12:52 +0530

    updated

--- Dynamic Programming/weirdAlgorithm.cpp
+++ Dynamic Programming/weirdAlgorithm.cpp
@@ -5,16 +5,21 @@
 
 using namespace std;
 
+vector<int> dp;
 int solve(int n){
     if(n == 1) return 0;
     if(n == 2 || n == 3) return 1;
 
-    return 1 + min({solve(n - 1),(n % 2 == 0) ? solve(n / 2) : inf,(n % 3 == 0) ? solve(n / 3) : inf});
+
+    if(dp[n] != -1) return dp[n];
+
+    return dp[n] = 1 + min({solve(n - 1),(n % 2 == 0) ? solve(n / 2) : inf,(n % 3 == 0) ? solve(n / 3) : inf});
 }
 
 int main(){
     int n;
     cin >> n;
+    dp.resize(n + 1,-1);
 
     cout << solve(n) << endl;
 }
