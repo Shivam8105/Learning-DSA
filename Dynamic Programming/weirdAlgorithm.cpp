1adda0c3970cf07616c156f0242a52a77f07597e
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2025-12-14 17:24:42 +0530

    updated

---
+++ Dynamic Programming/weirdAlgorithm.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+#define inf INT_MAX
+
+using namespace std;
+
+int solve(int n){
+    if(n == 1) return 0;
+    if(n == 2 || n == 3) return 1;
+
+    return 1 + min({solve(n - 1),(n % 2 == 0) ? solve(n / 2) : inf,(n % 3 == 0) ? solve(n / 3) : inf});
+}
+
+int main(){
+    int n;
+    cin >> n;
+
+    cout << solve(n) << endl;
+}
