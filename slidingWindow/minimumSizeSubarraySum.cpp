9b8438e0f55516b363dcc64a3400ed33954ff2de
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2026-02-02 22:30:03 +0530

    updated

--- slidingWindow/minimumSizeSubarraySum.cpp
+++ slidingWindow/minimumSizeSubarraySum.cpp
@@ -6,17 +6,13 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
-
     vector<int> nums(n);
     for(int i = 0; i < n; i++){
         cin >> nums[i];
     }
-
     int target;
     cin >> target;
-
     int len = INT_MAX;
-
     int i = 0;
     int j = 0;
     int sum = 0;
