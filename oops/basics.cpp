7da4cad786ec905698b77333713e1cce4754d9e7
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2026-02-03 13:19:18 +0530

    updated

--- oops/basics.cpp
+++ oops/basics.cpp
@@ -8,15 +8,27 @@ public:
     string name;
     int rno;
     float gpa;
+    Student(string n,int r,float g){
+        name = n;
+        rno = r;
+        gpa = g;
+    }
 };
+
+void print(Student c){
+    cout << " " << c.name << " " << c.rno << " " <<  c.gpa << endl;
+}
 
 int main(){
     // Hero h1;
     // cout << "Size: " << sizeof(h1) << endl;
-    Student s;
-    s.name = "Shivam";
-    s.rno = 24;
-    s.gpa = 8.43;
+    Student s("Sumit chomu",24,8.52);
+    // s.name = "Shivam";
+    // s.rno = 24;
+    // s.gpa = 8.43;
 
-    cout << s.name << " " << s.rno << " " << s.gpa << " " << endl;
+    print(s);
+
+    // cout << s.name << " " << s.rno << " " << s.gpa << " " << endl;
+
 }
