c2905385eb1f202221e672ce3e2bb90f6a96656c
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2026-02-07 16:16:11 +0530

    updated

--- oops/thisKeyword.cpp
+++ oops/thisKeyword.cpp
@@ -3,13 +3,39 @@
 
 using namespace std;
 
-class Student{
+class Cricketer{
 public:
-    int age;
+    string name;
+    int runs;
+
+    //! normal pointing without using the this keyword
+    // Cricketer(string s, int r){
+    //     name = s;
+    //     runs = r;
+    // }
+
+    //! this operator signifies that it actally points to the data members of the class when an object is calling any function. // basically it points  to the current object.
+
+    Cricketer(string name,int runs){
+        this -> name = name;
+        this -> runs = runs;
+    }
+
+    //! it will give garbage value because it doesn't know where to signify
+    // Cricketer(string name,int runs){
+    //     name = name;
+    //     runs = runs;
+    // }
 };
 
+void print(Cricketer c){
+    cout << c.name << " " << c.runs << endl;
+}
+
 int main(){
-    Student s;
-    s.age = 10;
-    // cout << s.age << endl;
+    Cricketer c1("Virat Kohli",25000);
+    Cricketer c2("Rohit Sharma",18000);
+
+    print(c1);
+    print(c2);
 }
