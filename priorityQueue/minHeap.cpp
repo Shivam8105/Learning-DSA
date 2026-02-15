0f545787b6a0747e1b89d659fe25e7793bd0edb7
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2026-02-15 22:15:34 +0530

    updated

---
+++ priorityQueue/minHeap.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    priority_queue<int,vector<int>,greater<int>> pq;
+
+    pq.push(10);
+    pq.push(30);
+    pq.push(50);
+    pq.push(20);
+    pq.push(40);
+
+    cout << pq.size() << endl;
+    while(!pq.empty()){
+        cout << pq.top() << " ";
+        pq.pop();
+    }
+    cout << endl;
+}
