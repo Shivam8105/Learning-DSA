f635d8fd529a3f097b1b12633298e9d3a3725186
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2026-02-15 20:52:34 +0530

    updated

---
+++ contest/ques2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while(t--) {
+        int n;
+        cin >> n;
+
+        vector<int> arr(n + 1);
+
+        for(int i = 1; i <= n; i++) {
+            cin >> arr[i];
+        }
+
+        bool possible = true;
+
+        for(int i = 1; i <= n; i++) {
+            int x = i;
+            int y = arr[i];
+            
+            while(x % 2 == 0) {
+                x /= 2;
+            }
+
+            while(y % 2 == 0) {
+                y /= 2;
+            }
+
+            if(x != y) {
+                possible = false;
+                break;
+            }
+        }
+
+        if(possible)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+
+    return 0;
+}
