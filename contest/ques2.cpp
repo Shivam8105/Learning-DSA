66c2562fa515f5b113c254f642fea24daace970a
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2026-02-15 20:53:59 +0530

    updated

--- contest/ques2.cpp
+++ contest/ques2.cpp
@@ -9,19 +9,16 @@ int main() {
     while(t--) {
         int n;
         cin >> n;
-
         vector<int> arr(n + 1);
-
         for(int i = 1; i <= n; i++) {
             cin >> arr[i];
         }
-
         bool possible = true;
-
+        
         for(int i = 1; i <= n; i++) {
             int x = i;
             int y = arr[i];
-            
+
             while(x % 2 == 0) {
                 x /= 2;
             }
