95a6b8e02531bd48516531d70ef2a3a064cb9602
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2026-02-18 16:44:49 +0530

    updated

--- Dynamic Programming/DigitDP/bennyAndTheBrokenOdometer.cpp
+++ Dynamic Programming/DigitDP/bennyAndTheBrokenOdometer.cpp
@@ -6,8 +6,35 @@
 
 using namespace std;
 
+int solve(string& num,int idx,int tight){
+    if(idx == num.size()){
+        return 1;
+    }
+    int lb = 0;
+    int ub = (tight == 1) ? num[idx] - '0' : 9;
+
+    int res = 0;
+    for(int digit = lb; digit <= ub; digit++){
+        int newTight = (tight == 1 && digit == ub) ? 1 : 0;
+       
+        if(digit == 3) continue;
+
+        res += solve(num,idx + 1,newTight);
+    }
+    return res;
+}
+
 
 int main(){
-    int n;
-    
+    int tc;
+    cin >> tc;
+
+    while(tc--){
+        int n;
+        cin >> n;
+
+        string num = to_string(n);
+
+        cout << solve(num,0,1) - 1 << endl;
+    }
 }
