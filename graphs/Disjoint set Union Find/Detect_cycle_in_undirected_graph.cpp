aa6b7a6fc9bdca9afce68331716ed98b2063425e
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2026-02-19 16:39:33 +0530

    GFG

---
+++ graphs/Disjoint set Union Find/Detect_cycle_in_undirected_graph.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int find(int x,vector<int>& parent){
+    if(x == parent[x]){
+        return x;
+    }
+    return parent[x] = find(parent[x],parent);
+}
+
+void Union(int x,int y, vector<int>& parent, vector<int>& rank){
+    int x_parent = find(x,parent);
+    int y_parent = find(y,parent);
+
+    if(x_parent == y_parent){
+        return;
+    }
+
+    if(rank[x_parent] > rank[y_parent]){
+        parent[y_parent] = x_parent;
+    }else if(rank[x_parent] < rank[y_parent]){
+        parent[x_parent] = y_parent;
+    }else{
+        parent[x_parent] = y_parent;
+        rank[y_parent]++;
+    }
+}
+
+int main(){
+    int n,m;
+    cin >> n >> m;
+
+    vector<int> parent(n + 1);
+    vector<int> rank(n + 1,0);
+
+    for(int i = 0; i <= n; i++){
+        parent[i] = i;
+    }
+
+    for(int i = 0; i < m; i++){
+        int u,v;
+        cin >> u >> v;
+
+        if(u < v){
+            int u_parent = find(u,parent);
+            int v_parent = find(v,parent);
+
+            if(u_parent != v_parent){
+                Union(u,v,parent,rank);
+            }else{
+                cout << "true" << endl;
+                return 0;
+            }
+        }
+    }
+    cout << "false" << endl;
+    return 0;
+}
