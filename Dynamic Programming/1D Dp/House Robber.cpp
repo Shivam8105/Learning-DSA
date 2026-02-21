f06e534793b2d3b22724d06f73c69e9ce21aa2a4
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2026-02-21 16:21:02 +0530

    Leetcode 198

---
+++ Dynamic Programming/1D Dp/House Robber.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+
+        int even = 0;
+        int odd = 0;
+
+        for(int i = 0; i < n; i++){
+            if(i % 2 == 0){
+                even = max(nums[i] + even,odd);
+            }else{
+                odd = max(nums[i] + odd,even);
+            }
+        }
+        return max(even,odd);
+    }
+};
