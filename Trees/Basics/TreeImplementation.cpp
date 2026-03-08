bebcf5addde720ccac69f8bf42ea5262f1c08410
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2026-03-08 20:36:13 +0530

    Basics

---
+++ Trees/Basics/TreeImplementation.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int data){
+        this -> data = data;
+        this -> left = nullptr;
+        this -> right = nullptr;
+    }
+};
+
+void display(Node* root){
+    if(root == nullptr){
+        return;
+    }
+    cout << root -> data << " ";
+    display(root -> left);
+    display(root -> right);
+}
+
+int main(){
+    Node* a = new Node(1);
+    Node* b = new Node(2);
+    Node* c = new Node(3);
+    Node* d = new Node(4);
+    Node* e = new Node(5);
+    Node* g = new Node(7);
+    Node* h = new Node(8);
+    a -> left = b;
+    a -> right = c;
+    b -> left = d;
+    b -> right = e;
+    c -> left = g;
+    c -> right = h;
+    display(a);
+}
