ffe42ad9b88ee21df8c88507b4be60a9c5e9720e
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2026-04-12 19:21:52 +0530

    Updated

--- oops/inheritance.cpp
+++ oops/inheritance.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
 using namespace std;
 
+class Scooty{
+public:
+    int topSpeed;
+    float mileage;
+protected:
+    int bootSpace; 
+};
+
+class Bike : public Scooty{
+public:
+    int gears;
+
+    void setBootspace(){
+        bootSpace = 70;
+    }
+    int getBootspace(){
+        return bootSpace;
+    }
+};
+
+void print(Bike &s){
+    cout << s.topSpeed << " " << s.mileage << " " << s.gears << " " << s.getBootspace() << endl;
+}
+
 int main(){
+    Bike b1;
+    b1.topSpeed = 180;
+    b1.mileage = 12.5;
+    b1.gears = 6;
+    b1.setBootspace();
+    print(b1);
     
+    // b1.bootSpace = 2;  !// not accessible in main function as it is protected
+
+
 }
