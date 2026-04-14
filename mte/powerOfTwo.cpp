8cb1a850405b4e6a8bd60a4b7466aaffd93e1599
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2026-04-14 12:57:28 +0530

    updated

--- mte/powerOfTwo.cpp
+++ mte/powerOfTwo.cpp
@@ -3,9 +3,21 @@
 #include <iostream>
 using namespace std;
 
+bool isPower(int n){
+    if(n == 1) return true;
+    if(n == 2) return true;
+
+    while(n > 2){
+        if(n % 2 != 0) return false;
+        n /= 2;
+    }
+    return true;
+}
+
 int main(){
     int n;
     cin >> n;
 
-    
+    bool ans = isPower(n);
+    cout << ans << endl;
 }
