bae6bedc6012c0a0773fcd06d95a2b7fa48a7d1e
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2026-04-14 13:12:25 +0530

    Leetcode 231

--- mte/powerOfTwo.cpp
+++ mte/powerOfTwo.cpp
@@ -1,18 +1,31 @@
 //! Check if a number is a power of two using bit manipulation.
 
+
+// O(log n) approach
+
 #include <iostream>
 using namespace std;
 
-bool isPower(int n){
-    if(n <= 0) return false;
-    if(n == 1) return true;
-    if(n == 2) return true;
+// bool isPower(int n){
+//     if(n <= 0) return false;
+//     if(n == 1) return true;
+//     if(n == 2) return true;
 
-    while(n > 2){
-        if(n % 2 != 0) return false;
-        n /= 2;
+//     while(n > 2){
+//         if(n % 2 != 0) return false;
+//         n /= 2;
+//     }
+//     return true;
+// }
+
+
+// O(n) approach
+
+bool isPower(int n){
+    if(n > 0 && (n & (n - 1)) == 0){
+        return true;
     }
-    return true;
+    return false;
 }
 
 int main(){
