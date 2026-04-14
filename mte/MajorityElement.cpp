930a6fe04ec04790849c1c77925aeba0de1d4fe5
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2026-04-14 12:03:13 +0530

    updated

--- mte/MajorityElement.cpp
+++ mte/MajorityElement.cpp
@@ -5,6 +5,32 @@
 #include <iostream>
 using namespace std;
 
+// int main(){
+//     int n;
+//     cin >> n;
+
+//     vector<int> nums(n);
+//     for(int i = 0; i < n; i++){
+//         cin >> nums[i];
+//     }
+
+//     unordered_map<int,int> mp;
+//     for(auto it : nums){
+//         mp[it]++;
+//     }
+
+//     for(auto it : mp){
+//         if(it.second > n / 2){
+//             cout << it.first << endl;
+//             return 0;
+//         }
+//     }
+//     cout << -1 << endl;
+//     return 0;
+// }
+
+//! boyer-moore voting algorithm
+
 int main(){
     int n;
     cin >> n;
@@ -13,21 +39,19 @@ int main(){
     for(int i = 0; i < n; i++){
         cin >> nums[i];
     }
+    int num = nums[0];
+    int count = 1;
 
-    unordered_map<int,int> mp;
-    for(auto it : nums){
-        mp[it]++;
-    }
-
-    for(auto it : mp){
-        if(it.second > n / 2){
-            cout << it.first << endl;
-            return 0;
+    for(int i = 1; i < n; i++){
+        if(num == nums[i]){
+            count++;
+        }else{
+            count--;
+        }
+        if(count == 0){
+            num = nums[i];
+            count = 1;
         }
     }
-    cout << -1 << endl;
-    return 0;
+    cout << num << endl;
 }
-
-//! boyer-moore voting algorithm
-
