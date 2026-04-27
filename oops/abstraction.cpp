3a96d5a43001ec224907b5173c3c4b92a987c58d
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2026-04-27 15:57:03 +0530

    updated

--- oops/abstraction.cpp
+++ oops/abstraction.cpp
@@ -1,24 +1,57 @@
+// #include <iostream>
+// using namespace std;
+
+// class Base{
+// public:
+//     virtual void show(){
+//         cout << "Base\n";
+//     }
+// };
+
+// class Derived : public Base{
+// public:
+//     void show(){
+//         cout << "Derived\n";
+//     }
+// };
+
+// int main(){
+//     Base* b;
+//     Derived d;
+
+//     b = &d;
+//     b->show();   // ✅ Output: Derived
+// }
+
+
 #include <iostream>
 using namespace std;
 
-class Base{
+class Shape{
 public:
-    virtual void show(){
-        cout << "Base\n";
+    virtual void draw() = 0;
+};
+
+class Circle : public Shape{
+public:
+    void draw(){
+        cout << "Circle has been drawn" << endl;
     }
 };
 
-class Derived : public Base{
+class Rectangle : public Shape{
 public:
-    void show(){
-        cout << "Derived\n";
+    void draw(){
+        cout << "Rectangle has been drawn" << endl;
     }
 };
 
 int main(){
-    Base* b;
-    Derived d;
-
-    b = &d;
-    b->show();   // ✅ Output: Derived
+    // Shape* s = new Circle;
+    Shape* s;
+    Circle c;
+    Rectangle r;
+    s = &c;
+    s = &r;
+    s -> draw();
 }
