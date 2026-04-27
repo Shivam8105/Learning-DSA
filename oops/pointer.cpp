17dc3274087d9537627fd01144362c2bd6e67578
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2026-04-27 15:48:58 +0530

    updated

--- oops/pointer.cpp
+++ oops/pointer.cpp
@@ -9,6 +9,11 @@ public:
 int main() {
     Student s1;
     s1.marks = 90;
+    int arr[3] = {10,20,30};
+    int* p = arr; 
+    cout << *p << endl;
+    cout << *(p + 1) << endl;
+    cout << *(p + 2) << endl;
 
     Student *ptr = &s1;   // object pointer
 
