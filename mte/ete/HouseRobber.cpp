842bd098912918bdf31996d87bbb1db142a9a523
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2026-06-16 17:31:24 +0530

    Leetcode 198

--- mte/ete/HouseRobber.cpp
+++ mte/ete/HouseRobber.cpp
@@ -1,19 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int rob(vector<int>& nums){
-    int n = nums.size();
-    int odd = 0;
-    int even = 0;
+// int rob(vector<int>& nums){
+//     int n = nums.size();
+//     int odd = 0;
+//     int even = 0;
 
-    for(int i = 0; i < n; i++){
-        if(i % 2 == 0){
-            even = max({even,even + nums[i],odd});
-        }else{
-            odd = max({odd,odd + nums[i],odd});
-        }
+//     for(int i = 0; i < n; i++){
+//         if(i % 2 == 0){
+//             even = max({even,even + nums[i],odd});
+//         }else{
+//             odd = max({odd,odd + nums[i],odd});
+//         }
+//     }
+//     return max(even,odd);
+// }
+
+int solve(int i, vector<int>& nums){
+    int n = nums.size();
+    if(i >= n){
+        return 0;
     }
-    return max(even,odd);
+
+    int skip = solve(i + 1,nums);
+    int take = nums[i] + solve(i + 2,nums);
+    return max(take,skip);
+}
+
+int rob(vector<int>& nums){
+    return solve(0,nums);
 }
 
 int main(){
