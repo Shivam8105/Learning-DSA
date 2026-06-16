f7caae018e6ed943bc74b1811a2cf8d3e3ab0950
Shivam Kumar <148937418+Shivam8105@users.noreply.github.com>
2026-06-16 11:01:24 +0530

    Leetcode 733

---
+++ mte/ete/FloodFill.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+vector<vector<int>> floodFill(vector<vector<int>>& image,int sr, int sc, int color){
+    int n = image.size();
+    int m = image[0].size();
+
+    queue<pair<int,int>> q;
+
+    if(image[sr][sc] == color){
+        return image;
+    }
+
+    int original = image[sr][sc];
+    image[sr][sc] = color;
+    
+    q.push({sr,sc});
+
+    vector<int> dRow = {-1,0,+1,0};
+    vector<int> dCol = {0,-1,0,+1};
+
+    while(!q.empty()){
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+
+        for(int i = 0; i < 4; i++){
+            int nx = x + dRow[i];
+            int ny = y + dCol[i];
+
+            if(nx >= 0 && nx < n && ny >= 0 && ny < m && image[nx][ny] == original){
+                image[nx][ny] = color;
+                q.push({sr,sc});
+            }
+        }
+    }
+    return image;
+}
+
+int main(){
+
+}
